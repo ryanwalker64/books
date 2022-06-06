e04b7e26225256031f3666e6ad3ebea6c35f3f29
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-06-06 17:22:50 +1200

    books modal working

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const bookCollection = document.querySelector('.bookCollection');
 const modal = document.querySelector('.modal-outer');
 const newBookBtn = document.querySelector('#addBook');
+const bookForm = document.querySelector('#newBookFormSubmit');
+const bookFormTitle = document.querySelector('.bookFormInput[name="title"]');
+const bookFormAuthor = document.querySelector('.bookFormInput[name="author"]');
+const bookFormPages = document.querySelector('.bookFormInput[name="pages"]');
+const bookFormFinished = document.querySelector('.bookFormInput[name="finished"]');
 
 newBookBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
@@ -9,9 +14,13 @@ newBookBtn.addEventListener('click', () => {
 modal.addEventListener('click', (e) => {
     // console.log(e.target.className)
     if (e.target.className == 'modal-outer') {
-        modal.style.display = 'none';
+        closeModal()
     }
 })
+
+function closeModal() {
+    modal.style.display = 'none';
+}
 
 let myLibrary = [
     {
@@ -30,13 +39,14 @@ let myLibrary = [
     },
 ];
 
-function Book(){
+function Book(title, author, pages, finished){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.finished = finished;
 
 }
 
-function addBookToLibrary(){
-
-}
 
 function diplayBooks(Library){
    const booksHTML = Library.map((book, index) => {
@@ -63,4 +73,19 @@ function diplayBooks(Library){
 
 }
 
+
+
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // console.log(bookFormTitle.value, bookFormAuthor.value )
+    const newbook = new Book(bookFormTitle.value, bookFormAuthor.value, bookFormPages.value, bookFormFinished.checked);
+    myLibrary.push(newbook)
+    diplayBooks(myLibrary);
+    closeModal()
+})
+
 diplayBooks(myLibrary);
+
+// remove book from list
+// switch finished or not
+// reset modal form
